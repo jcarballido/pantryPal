4b3428bb70fb37d0abf232990f491f6872c9bedb
carbDev <carballidoj92@gmail.com>
2024-12-03 23:39:30 -0600

    Created initial tab files.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Tabs } from 'expo-router'
 
 export default function TabsLayout() {
   return (
-    <View>
-      <Text>TabsLayout</Text>
-    </View>
+    <Tabs>
+      <Tabs.Screen name='index' options={{ title: 'Home' }} />
+      <Tabs.Screen name='search' options={{ title: 'Search' }} />
+      <Tabs.Screen name='list' options={{ title: 'Shopping List' }} />
+      <Tabs.Screen name='account' options={{ title: 'Account' }} />
+    </Tabs>
   )
 }
